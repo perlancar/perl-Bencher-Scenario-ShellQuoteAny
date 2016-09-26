0b0ad8dd059a8bd31ebc2a7159bdac60c1e4a1ec
perlancar (@pc-office) <perlancar@gmail.com>
2016-09-26 12:12:06 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/ShellQuoteAny.pm
@@ -0,0 +1,33 @@
+package Bencher::Scenario::ShellQuoteAny;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Benchmark cross-platform shell quoting',
+    participants => [
+        {
+            fcall_template=>'ShellQuote::Any::shell_quote(<cmd>)',
+        },
+        {
+            fcall_template=>'PERLANCAR::ShellQuote::Any::shell_quote(@{<cmd>})',
+        },
+    ],
+    datasets => [
+        {name=>'empty0', args=>{cmd=>[]}},
+        {name=>'empty1', args=>{cmd=>['']}},
+        {name=>'cmd1', args=>{cmd=>['foo bar']}},
+        {name=>'cmd5', args=>{cmd=>['foo', 'bar', 'baz', 'qux', 'quux']}},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 SEE ALSO
+
+L<Bencher::Scenario::HashBuilding>
